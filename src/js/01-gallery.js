b3157a81a0d450152c241bcb3c723630aab123c9
Mihai Manole <mesterum@users.noreply.github.com>
2024-01-03 21:11:13 +0000

    Exercițiul 1 - librăria SimpleLightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('ul.gallery'),
+  liTemplate = new DOMParser().parseFromString(
+    `<li class="gallery__item">
+   <a class="gallery__link">
+      <img class="gallery__image" />
+   </a>
+</li>`,
+    'text/html'
+  ).body.firstElementChild;
+
+function liForEachGalleryItem(item) {
+  const clone = liTemplate.cloneNode(true),
+    img = clone.querySelector('img'),
+    a = clone.querySelector('a');
+
+  img.src = item.preview;
+  img.alt = item.description;
+  a.href = item.original;
+  return clone;
+}
+
+gallery.append(...galleryItems.map(liForEachGalleryItem));
+
+new SimpleLightbox('ul.gallery a', { captionsData: 'alt', captionDelay: 250 });
