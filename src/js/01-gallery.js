095dfb5ecca092f38fe005ad1dccf4b27074cc56
Mihai Manole <mesterum@users.noreply.github.com>
2024-01-04 21:28:56 +0000

    Exercițiul 2 - video player

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,20 +4,20 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-const gallery = document.querySelector('ul.gallery'),
-  liTemplate = new DOMParser().parseFromString(
-    `<li class="gallery__item">
-   <a class="gallery__link">
+const gallery = document.querySelector('ul.gallery');
+const liTemplate = new DOMParser().parseFromString(
+  `<li class="gallery__item">
+    <a class="gallery__link">
       <img class="gallery__image" />
-   </a>
-</li>`,
-    'text/html'
-  ).body.firstElementChild;
+    </a>
+  </li>`,
+  'text/html'
+).body.firstElementChild;
 
 function liForEachGalleryItem(item) {
-  const clone = liTemplate.cloneNode(true),
-    img = clone.querySelector('img'),
-    a = clone.querySelector('a');
+  const clone = liTemplate.cloneNode(true);
+  const img = clone.querySelector('img');
+  const a = clone.querySelector('a');
 
   img.src = item.preview;
   img.alt = item.description;
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+const key = 'videoplayer-current-time';
+const initTime = localStorage.getItem(key);
+
+player.on(
+  'timeupdate',
+  throttle(time => localStorage.setItem(key, time.seconds), 1000, {
+    leading: false,
+  })
+);
+
+if (initTime != null) {
+  player.setCurrentTime(+initTime);
+}
