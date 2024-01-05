6959c3e6af6b7a9e56f0d77d0402e962fdab1338
Mihai Manole <mesterum@users.noreply.github.com>
2024-01-05 14:49:31 +0000

    Exercițiul 3 - formularul pentru feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+/**
+ * @type {HTMLFormElement} form
+ */
+const form = document.querySelector('form.feedback-form');
+/** @type {{email: HTMLInputElement, message: HTMLTextAreaElement}} */
+const { email, message } = form.elements;
+const key = 'feedback-form-state';
+const formData = localStorage.getItem(key);
+/** @param {Event} _e */
+const saveForm = _e => {
+  localStorage.setItem(
+    key,
+    JSON.stringify({
+      email: email.value,
+      message: message.value,
+    })
+  );
+};
+
+if (formData != null) {
+  const parsedFormData = JSON.parse(formData);
+  for (const key in parsedFormData) {
+    form[key].value = parsedFormData[key];
+  } /* 
+  Object.entries(JSON.parse(formData)).forEach(([name, value]) => {
+    form[name].value = value;
+  }); */
+}
+email.addEventListener('input', throttle(saveForm, 500, { leading: false }));
+message.addEventListener('input', throttle(saveForm, 500, { leading: false }));
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(key)));
+  localStorage.removeItem(key);
+  form.reset();
+});
